19463b261bf1d4973b8cd56bcc99583d804a6e36
Rostislav Gubkin <rostislavlp@gmail.com>
2024-07-13 22:35:47 +0300

    start project

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
+type Repositoty struct {
+	DB *gorm.DB
+}
+
+func (r *Repositoty) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("/delete_book/:id", r.DeleteBook)
+	api.Get("/get_books/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("could not load the database")
+	}
+
+	r := Repositoty{
+		DB: db,
+	}
+	app := fiber.New()
+
+	app.Listen(":8080")
+}
